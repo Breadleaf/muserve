81c7bb43e324c0efa6af0506dbdd0970fe7fb97f
breadleaf <bkhnapa@gmail.com>
2025-09-12 17:44:05 -0700

    prepare to remove https/tls from internal docker network

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -4,5 +4,5 @@ bind = "0.0.0.0:8000"
 
 workers = multiprocessing.cpu_count() * 2 + 1
 
-certfile = "./server.crt"
-keyfile = "./server.key"
+#certfile = "./server.crt"
+#keyfile = "./server.key"
--- auth/gunicorn.conf.py
+++ auth/gunicorn.conf.py
@@ -3,3 +3,6 @@ import multiprocessing
 bind = "0.0.0.0:7000"
 
 workers = multiprocessing.cpu_count() * 2 + 1
+
+#certfile = "./server.crt"
+#keyfile = "./server.key"
