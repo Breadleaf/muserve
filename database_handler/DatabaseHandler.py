7d6b68f31b84fe140e9635d0f27a08ab7a8d7352
Breadleaf <bkhnapa@gmail.com>
2025-09-15 11:27:59 -0700

    isolate database into new container

---
+++ database_handler/DatabaseHandler.py
@@ -0,0 +1,57 @@
+import psycopg2
+
+import os
+import sys
+import urllib.parse as up
+
+DATABASE_URL = os.environ.get("DATABASE_URL", None)
+
+class DatabaseHandler:
+    def __init__(self):
+        if not DATABASE_URL:
+            print(f"envvar DATABASE_URL doesn't exist", file=sys.stderr)
+            sys.exit(1)
+
+        parsed_url = up.urlparse(DATABASE_URL)
+
+        try:
+            self.con = psycopg2.connect(
+                database=parsed_url.path[1:],
+                user=parsed_url.username,
+                password=parsed_url.password,
+                host=parsed_url.hostname,
+                port=parsed_url.port
+            )
+
+        except psycopg2.Error as e:
+            print(
+                f"error connecting to PostgreSQL server: {e}",
+                file=sys.stderr
+            )
+            sys.exit(1)
+
+    def fetch(self, query, values=()):
+        try:
+            cur = self.con.cursor()
+            cur.execute(query, values)
+            res = cur.fetchall()
+
+            # return (True, result) for golang-like error handling
+            return (True, res)
+        
+        except psycopg2.Error as e:
+            # return (False, error) for golang-like error handling
+            return (False, f"db error from fetch(): {e}")
+
+    def insert(self, query, values=()):
+        try:
+            cur = self.con.cursor()
+            cur.execute(query, values)
+            self.con.commit()
+            cur.close()
+
+            # return no error
+            return None
+
+        except psycopg2.Error as e:
+            return f"db error from insert(): {e}"
